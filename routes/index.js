c1fb18a8733eab22d166ecbe39ea1c9d3adb822a
master-dhanesh <dhanesh1296@gmail.com>
2024-03-15 20:10:31 +0530

    reading file

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,23 @@ const globalpath = path.join(__dirname, "../", "public", "uploads");
 
 router.get("/", function (req, res, next) {
     const files = fs.readdirSync(globalpath);
-    res.render("index", { files: files });
+    res.render("index", { files: files, filedata: "" });
 });
 
-router.get("/:filename", function (req, res, next) {
+router.get("/file/:filename", function (req, res, next) {
+    const filedata = fs.readFileSync(
+        path.join(globalpath, req.params.filename),
+        "utf-8"
+    );
     const files = fs.readdirSync(globalpath);
-    res.render("index", { files: files });
+    res.render("index", { files: files, filedata: filedata });
 });
 
 router.post("/createfile", function (req, res, next) {
     // const filename = req.body.filename;
     const { filename } = req.body;
     fs.writeFileSync(path.join(globalpath, filename), "");
-    res.redirect(`/${filename}`);
+    res.redirect(`/file/${filename}`);
 });
 
 module.exports = router;
